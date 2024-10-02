7a2c11921c43fba713b7031e08c10513904923f8
Katu <captain.katu@gmail.com>
2024-10-01 19:07:46 -0500

    1.16.14 (10/1/24)
    - Slightly buffed spawn rate of vanilla Turtles
    - Aquaculture Turtles can now be fished up! Box Turtles in Swamps, Arrau Turtles in Oceans, and Starshell Turtles in the Twilight Forest!
    - Fixed an issue with fishing up hat bags.

--- kubejs/server_scripts/loot_editing/fishing_loot.js
+++ kubejs/server_scripts/loot_editing/fishing_loot.js
@@ -1,43 +1,86 @@
 LootJS.modifiers((event) => {
+
+    //Turtles can be fished up
     event.addLootTableModifier("minecraft:gameplay/fishing")
+    .logName("box_turtle")
+    .anyBiome("minecraft:swamp", "minecraft:mangrove_swamp", "#forge:is_swamp")
     .randomChance(0.20)
-    .addLoot("simplehats:hatbag_common")
-
+    .removeLoot(Ingredient.all)
+    .addLoot("aquaculture:box_turtle")
+    
     event.addLootTableModifier("minecraft:gameplay/fishing")
-    .randomChance(0.15)
-    .addLoot("simplehats:hatbag_uncommon")
-
-    event.addLootTableModifier("minecraft:gameplay/fishing/")
-    .randomChance(0.10)
-    .addLoot("simplehats:hatbag_rare")
-
-    event.addLootTableModifier("minecraft:gameplay/fishing/")
+    .anyBiome("#forge:is_ocean")
+    .logName("arrau_turtle")
     .randomChance(0.02)
-    .addLoot("simplehats:hatbag_epic")
+    .removeLoot(Ingredient.all)
+    .addLoot("aquaculture:arrau_turtle")
 
-    event.addLootTableModifier("minecraft:gameplay/fishing/junk/")
+    event.addLootTableModifier("minecraft:gameplay/fishing")
+    .anyBiome("#twilightforest:in_twilight_forest")
+    .logName("starshell_turtle")
+    .randomChance(0.02)
+    .removeLoot(Ingredient.all)
+    .addLoot("aquaculture:starshell_turtle")
+
+    //Hat Bags
+    event.addLootTableModifier("minecraft:gameplay/fishing")
+    .logName("hatbag_common_fishing")
     .randomChance(0.20)
+    .removeLoot(Ingredient.all)
     .addLoot("simplehats:hatbag_common")
 
-    event.addLootTableModifier("minecraft:gameplay/fishing/junk/")
+    event.addLootTableModifier("minecraft:gameplay/fishing")
+    .logName("hatbag_uncommon_fishing")
     .randomChance(0.15)
+    .removeLoot(Ingredient.all)
+    .addLoot("simplehats:hatbag_uncommon")
+
+    event.addLootTableModifier("minecraft:gameplay/fishing")
+    .logName("hatbag_rare_fishing")
+    .randomChance(0.10)
+    .removeLoot(Ingredient.all)
+    .addLoot("simplehats:hatbag_rare")
+
+    event.addLootTableModifier("minecraft:gameplay/fishing")
+    .logName("hatbag_epic_fishing")
+    .randomChance(0.02)
+    .removeLoot(Ingredient.all)
+    .addLoot("simplehats:hatbag_epic")
+
+    //Katu thinks these other loot tables literally aren't getting used and she thinks it's probably Aquaculture's fault.
+    /*
+    event.addLootTableModifier("minecraft:gameplay/fishing/junk")
+    .logName("hatbag_common_junk")
+    .randomChance(0.20)
+    .removeLoot(Ingredient.all)
+    .addLoot("simplehats:hatbag_common")
+
+    event.addLootTableModifier("minecraft:gameplay/fishing/junk")
+    .logName("hatbag_uncommon_junk")
+    .randomChance(0.15)
+    .removeLoot(Ingredient.all)
     .addLoot("simplehats:hatbag_uncommon")
 
     event.addLootTableModifier("minecraft:gameplay/fishing/treasure")
     .randomChance(0.20)
+    .logName("hatbag_rare_treasure")
     .addLoot("simplehats:hatbag_rare")
 
     event.addLootTableModifier("minecraft:gameplay/fishing/treasure")
+    .logName("hatbag_epic_treasure")
     .randomChance(0.10)
     .addLoot("simplehats:hatbag_epic")
 
     event.addLootTableModifier("aquaculture:gameplay/fishing/neptunium")
+    .logName("hatbag_rare_neptunium")
     .randomChance(0.20)
     .addLoot("simplehats:hatbag_rare")
 
     event.addLootTableModifier("aquaculture:gameplay/fishing/neptunium")
+    .logName("hatbag_epic_neptunium")
     .randomChance(0.10)
     .addLoot("simplehats:hatbag_epic")
+    */
 
 })
 
