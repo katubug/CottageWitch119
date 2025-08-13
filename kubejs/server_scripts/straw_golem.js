8520669d5eef5bd0d1648c17be8fdcdce6f3f267
earl624 <90653071+earl624@users.noreply.github.com>
2025-08-13 12:24:05 -0400

    Update straw_golem.js
    Fixed bug where placing a pumpkin on the side would delete blocks in undesired locations

--- kubejs/server_scripts/straw_golem.js
+++ kubejs/server_scripts/straw_golem.js
@@ -1,23 +1,32 @@
 /**
- * summon smol friend pumpkin
+ * Summons a pumpkin warden (or in our case, a 'straw golem') when
+ * placing a pumpkin next to a hay block
  */
-BlockEvents.rightClicked((event) => {
+BlockEvents.placed((event) => {
     const {
         block: { x, y, z },
         block,
         server,
-        item,
+        level,
     } = event;
-    if (item != "minecraft:carved_pumpkin") return;
-    if (!IsAdjacentClicked(event, "minecraft:carved_pumpkin", "minecraft:hay_block", "*"))
+
+    // Check for carved pumpy next to haybale
+    if (block != "minecraft:carved_pumpkin") return;
+    if (!IsAdjacentPlaced(event, "minecraft:carved_pumpkin", "minecraft:hay_block", "down"))
         return;
-    block.up.set("air");
-    block.set("air");
+
+    // Break blocks
+    level.destroyBlock(block.down.pos, false);
+    level.destroyBlock(block.pos, false);
+
+    // Create pumpkin warden
     let warden = block.createEntity("byg:pumpkin_warden");
-    warden.setPos(x, y, z);
+    warden.setPos(x, y-1, z);
     warden.spawn();
-    server.runCommandSilent(`particle heart ${x} ${y + 1} ${z} 1 1 1 0.1 10 normal`);
+
+    // Spawn particle and sound
+    level.spawnParticles("heart", true, x, y - 0.5, z, 0.3, 0.6, 0.3, 10, 0);
     server.runCommandSilent(
-        `playsound artifacts:generic.pop block @a ${x} ${y + 1} ${z} 1`
+        `playsound artifacts:generic.pop block @a ${x} ${y} ${z} 1`
     );
 });
