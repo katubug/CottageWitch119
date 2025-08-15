9e1aaee4e657c249f244be36912842b067929cf8
Astrofishisist <147417778+Astrofishisist@users.noreply.github.com>
2025-08-16 00:56:08 +0100

    goleme

--- kubejs/server_scripts/straw_golem.js
+++ kubejs/server_scripts/straw_golem.js
@@ -1,23 +1,29 @@
 /**
  * summon smol friend pumpkin
  */
-BlockEvents.rightClicked((event) => {
-    const {
-        block: { x, y, z },
-        block,
-        server,
-        item,
-    } = event;
-    if (item != "minecraft:carved_pumpkin") return;
-    if (!IsAdjacentClicked(event, "minecraft:carved_pumpkin", "minecraft:hay_block", "*"))
-        return;
-    block.up.set("air");
-    block.set("air");
-    let warden = block.createEntity("byg:pumpkin_warden");
-    warden.setPos(x, y, z);
-    warden.spawn();
-    server.runCommandSilent(`particle heart ${x} ${y + 1} ${z} 1 1 1 0.1 10 normal`);
-    server.runCommandSilent(
-        `playsound artifacts:generic.pop block @a ${x} ${y + 1} ${z} 1`
-    );
+BlockEvents.placed((event) => {
+	const {
+		block: { x, y, z },
+		block,
+		server,
+		level,
+	} = event;
+
+	// Check for carved pumpy next to haybale
+	if (block != "minecraft:carved_pumpkin") return;
+	if (!IsAdjacentPlaced(event, "minecraft:carved_pumpkin", "minecraft:hay_block", "down"))
+		return;
+
+	// Break blocks
+	level.destroyBlock(block.down.pos, false);
+	level.destroyBlock(block.pos, false);
+
+	// Create pumpkin warden
+	let warden = block.createEntity("byg:pumpkin_warden");
+	warden.setPos(x, y - 1, z);
+	warden.spawn();
+
+	// Spawn particle and sound
+	level.spawnParticles("heart", true, x, y - 0.5, z, 0.3, 0.6, 0.3, 10, 0);
+	server.runCommandSilent(`playsound artifacts:generic.pop block @a ${x} ${y} ${z} 1`);
 });
