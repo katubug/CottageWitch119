039a650315ac0373b141e307f2e0da5ae13a4fa8
Katu <captain.katu@gmail.com>
2026-03-11 12:31:58 -0500

    carvies

--- kubejs/server_scripts/forbidden_interactions/bad_carvies.js
+++ kubejs/server_scripts/forbidden_interactions/bad_carvies.js
@@ -2,13 +2,15 @@
  * Shattered dimensional carver teleports
  * you to the end of the world - bad idea
  */
-ItemEvents.rightClicked("alexsmobs:shattered_dimensional_carver", (e) => {
-	if (e.level.time % 20) {
-		// don't spam the console
-		WarnPlayer(
-			e,
-			"Don't use. To summon the Farseer, craft a Farseer Gate Pearl using this item."
-		);
-	}
-	e.cancel();
+ItemEvents.rightClicked(event => {
+  if (event.item.id != "alexsmobs:shattered_dimensional_carver") return;
+
+  if (event.level.time % 20 == 0) {
+    _WarnPlayer(
+      event,
+      "Don't use. To summon the Farseer, craft a Farseer Gate Pearl using this item."
+    );
+  }
+
+  event.cancel();
 });
